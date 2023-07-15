d1342344e787a09690e82cfd33733802b3a9dcb5
allea1999 <90058732+allea1999@users.noreply.github.com>
2023-07-14 21:53:20 -0400

    Add files via upload
    Employee Data am-commit

---
+++ M02-A01/src/main/java/EmployDetail.java
@@ -0,0 +1,22 @@
+/*
+ */
+
+/**
+ *
+ * @author alleam
+ */
+class EmployDetail {
+
+    String employ_id;
+    String name;
+    String employ_contact;
+    String employ_salary;
+    String email;
+    String position;
+    String father_name;
+
+    void getInfo() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+}
